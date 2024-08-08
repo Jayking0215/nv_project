91814f9a68dbdc720ff738a89d84931cb5cd7a9a
Jayking0215 <zaesoon2@naver.com>
2024-08-08 15:25:02 +0900

    Home init

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,71 +1,19 @@
+<!-- frontend/src/views/About.vue -->
 <template>
   <div>
-    <h1>To-Do List</h1>
-    <form @submit.prevent="addTodo">
-      <input v-model="newTodo" placeholder="Enter a new task" required />
-      <button type="submit">Add</button>
-    </form>
-    <ul>
-      <li v-for="todo in todos" :key="todo.id">
-        {{ todo.description }}
-        <button @click="deleteTodo(todo.id)">Delete</button>
-      </li>
-    </ul>
-    <div>
-      <router-link to="/main">Go to Main</router-link>
-      &nbsp;
-      <router-link to="/about">Go to About</router-link>
-      &nbsp;
-      <router-link to="/contact">Go to Contact</router-link>
-    </div>
+    <h1>Home</h1>
+    <p>This is CRM-Home with admin page & login</p>
+    <router-link to="/">Go to Main</router-link>
+    &nbsp;
+    <router-link to="/about">Go to About</router-link>
+    &nbsp;
+    <router-link to="/contact">Go to Contact</router-link>
+    &nbsp;
   </div>
 </template>
 
 <script>
 export default {
-  name: "MainPage",
-  data() {
-    return {
-      newTodo: "",
-      todos: [],
-    };
-  },
-  async created() {
-    await this.fetchTodos();
-  },
-  methods: {
-    async fetchTodos() {
-      try {
-        const response = await fetch("/api/todos");
-        this.todos = await response.json();
-      } catch (error) {
-        console.error("Error fetching todos:", error);
-      }
-    },
-    async addTodo() {
-      try {
-        const response = await fetch("/api/todos", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ description: this.newTodo }),
-        });
-        const newTodo = await response.json();
-        this.todos.push(newTodo);
-        this.newTodo = "";
-      } catch (error) {
-        console.error("Error adding todo:", error);
-      }
-    },
-    async deleteTodo(id) {
-      try {
-        await fetch(`/api/todos/${id}`, { method: "DELETE" });
-        this.todos = this.todos.filter((todo) => todo.id !== id);
-      } catch (error) {
-        console.error("Error deleting todo:", error);
-      }
-    },
-  },
+  name: "HomePage",
 };
 </script>
