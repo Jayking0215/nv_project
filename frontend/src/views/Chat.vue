b2a892ca53073a9c774410206d9655be6b36d6d5
Jayking0215 <zaesoon2@naver.com>
2024-08-23 23:06:38 +0900

    chat.vue 수정

--- frontend/src/views/Chat.vue
+++ frontend/src/views/Chat.vue
@@ -4,14 +4,14 @@
     <h1>Chatting</h1>
     <p>Real Time Chat service</p>
 
-    <div v-for="message in messages" :key="message.id">
-      {{ message.text }}
+    <div v-for="(message, index) in messages" :key="index">
+      {{ message }}
     </div>
 
-    <input
-      v-model="newMessage"
-      @keyup.enter="sendMessage"
-      placeholder="Type a message..."
+    <input 
+      v-model="newMessage" 
+      @keyup.enter="sendMessage" 
+      placeholder="Type a message..." 
     />
 
     <router-link to="/">Go to Main</router-link>
@@ -27,28 +27,35 @@ export default {
     return {
       socket: null,
       messages: [],
-      newMessage: "",
+      newMessage: ""
     };
   },
   created() {
     this.socket = io("http://localhost:3000");
-
+    
+    // 서버로부터 'chat message' 이벤트를 수신하여 메시지 배열에 추가
     this.socket.on("chat message", (msg) => {
-      this.messages.push({ id: this.messages.length, text: msg });
+      this.messages.push(msg);
     });
   },
   methods: {
     sendMessage() {
       if (this.newMessage.trim() !== "") {
+        // 서버로 'chat message' 이벤트 전송
         this.socket.emit("chat message", this.newMessage);
         this.newMessage = "";
       }
-    },
+    }
   },
   beforeDestroy() {
+    // 컴포넌트가 파괴되기 전에 소켓 연결 해제
     if (this.socket) {
       this.socket.disconnect();
     }
-  },
+  }
 };
 </script>
+
+<style scoped>
+/* 스타일은 필요에 따라 추가 */
+</style>
