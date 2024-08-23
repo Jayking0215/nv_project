ad6f0bfecd05b184dbfda61a95a93be737c4df0f
Jayking0215 <zaesoon2@naver.com>
2024-08-23 23:11:52 +0900

    chat.vue 보완

--- frontend/src/views/Chat.vue
+++ frontend/src/views/Chat.vue
@@ -8,11 +8,14 @@
       {{ message }}
     </div>
 
-    <input 
-      v-model="newMessage" 
-      @keyup.enter="sendMessage" 
-      placeholder="Type a message..." 
-    />
+    <div>
+      <input
+        v-model="newMessage"
+        placeholder="Type a message..."
+        @keyup.enter="sendMessage"
+      />
+      <button @click="sendMessage">Send</button>
+    </div>
 
     <router-link to="/">Go to Main</router-link>
   </div>
@@ -27,12 +30,12 @@ export default {
     return {
       socket: null,
       messages: [],
-      newMessage: ""
+      newMessage: "",
     };
   },
   created() {
     this.socket = io("http://localhost:3000");
-    
+
     // 서버로부터 'chat message' 이벤트를 수신하여 메시지 배열에 추가
     this.socket.on("chat message", (msg) => {
       this.messages.push(msg);
@@ -45,17 +48,29 @@ export default {
         this.socket.emit("chat message", this.newMessage);
         this.newMessage = "";
       }
-    }
+    },
   },
   beforeDestroy() {
     // 컴포넌트가 파괴되기 전에 소켓 연결 해제
     if (this.socket) {
       this.socket.disconnect();
     }
-  }
+  },
 };
 </script>
 
 <style scoped>
-/* 스타일은 필요에 따라 추가 */
+div {
+  margin-bottom: 10px;
+}
+
+input {
+  padding: 8px;
+  width: 200px;
+  margin-right: 10px;
+}
+
+button {
+  padding: 8px 12px;
+}
 </style>
